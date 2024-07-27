2c779b82506a1a283ee9ef8a5e5431d82a660c56
DazyLude <36658009+DazyLude@users.noreply.github.com>
2024-07-27 21:24:24 +0400

    added animation states for Alta and Arya added main menu music added 2 more levels

--- scripts/level_objects/arya_object.gd
+++ scripts/level_objects/arya_object.gd
@@ -11,6 +11,22 @@ var nudge_fx : AudioStream = null;
 var in_the_shadow : bool = false;
 var lit : bool = false;
 
+var powered_up : bool = false;
+
+var animation_state : int = IDLE_DOWN;
+
+enum {
+	STUCK,
+	WALK_LEFT,
+	WALK_RIGHT,
+	WALK_UP,
+	WALK_DOWN,
+	IDLE_LEFT,
+	IDLE_RIGHT,
+	IDLE_UP,
+	IDLE_DOWN,
+}
+
 
 func _init():
 	add_child(sfx_player);
@@ -24,6 +40,7 @@ func _init():
 	animated_sprite.sprite_frames = preload("res://assets/animations/arya_sprite_frames.tres");
 	move_fx = preload("res://assets/sfx/whoop_h.wav");
 	nudge_fx = preload("res://assets/sfx/poowh_h.wav");
+	margin_from_bottom = 10.;
 
 
 func _ready():
@@ -35,17 +52,8 @@ func _ready():
 	
 func _prepare_visuals() -> void:
 	var diff =  _level_ref.cell_size - self.animated_sprite.sprite_frames\
-		.get_frame_texture("walk", 0).get_size();
+		.get_frame_texture("idle_down", 0).get_size();
 	animated_sprite.offset = Vector2(diff.x / 2., diff.y - margin_from_bottom);
-
-
-func change_direction(new_direction: Direction) -> void:
-	if new_direction.num == Direction.LEFT && direction.num != Direction.LEFT:
-		animated_sprite.flip_h = true;
-	if new_direction.num == Direction.RIGHT && direction.num != Direction.RIGHT:
-		animated_sprite.flip_h = false;
-	if new_direction.num != Direction.NONE || !new_direction.equals(direction):
-		direction = new_direction;
 
 
 func nudge(from: Vector2, to: Vector2) -> void:
@@ -79,8 +87,10 @@ func _light_tick() -> bool:
 	
 	match [lit, has_tag(TAGS.STRONG)]:
 		[true, false]:
+			powered_up = true;
 			tags.push_back(TAGS.STRONG);
 		[false, true]:
+			powered_up = false;
 			tags.erase(TAGS.STRONG);
 	
 	super._light_tick();
@@ -101,3 +111,54 @@ func _turn_tick() -> void:
 	
 	if in_the_shadow:
 		stuck = true;
+
+
+func _process(delta: float) -> void:
+	var desired_state : int;
+	match [stuck, _movement_mode, _direction]:
+		[true, _, _]:
+			desired_state = STUCK;
+		# up
+		[false, _MOVE, Direction.UP], [false, _NUDGE, Direction.UP]:
+			desired_state = WALK_UP;
+		[false, _STOP, Direction.UP]:
+			desired_state = IDLE_UP;
+		# left
+		[false, _MOVE, Direction.LEFT], [false, _NUDGE, Direction.LEFT]:
+			desired_state = WALK_LEFT;
+		[false, _STOP, Direction.LEFT]:
+			desired_state = IDLE_LEFT;
+		# right
+		[false, _MOVE, Direction.RIGHT], [false, _NUDGE, Direction.RIGHT]:
+			desired_state = WALK_RIGHT;
+		[false, _STOP, Direction.RIGHT]:
+			desired_state = IDLE_RIGHT;
+		# down
+		[false, _MOVE, Direction.DOWN], [false, _NUDGE, Direction.DOWN]:
+			desired_state = WALK_DOWN;
+		[false, _STOP, Direction.DOWN]:
+			desired_state = IDLE_DOWN;
+	
+	if desired_state != animation_state:
+		animation_state = desired_state;
+		match desired_state:
+			STUCK:
+				animated_sprite.play(&"stuck");
+			WALK_LEFT:
+				animated_sprite.play(&"walk_left");
+			WALK_RIGHT:
+				animated_sprite.play(&"walk_right");
+			WALK_UP:
+				animated_sprite.play(&"walk_up");
+			WALK_DOWN:
+				animated_sprite.play(&"walk_down");
+			IDLE_LEFT:
+				animated_sprite.play(&"idle_left");
+			IDLE_RIGHT:
+				animated_sprite.play(&"idle_right");
+			IDLE_UP:
+				animated_sprite.play(&"idle_up");
+			IDLE_DOWN:
+				animated_sprite.play(&"idle_down");
+	
+	super._process(delta);
